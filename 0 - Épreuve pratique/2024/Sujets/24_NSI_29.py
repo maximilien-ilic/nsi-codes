91046195d784a7bbb3fffb50c2a19bf5de838120
maximilien-ilic <159456382+maximilien-ilic@users.noreply.github.com>
2024-04-27 19:29:00 +0200

    Update 24_NSI_29.py
    par maximilien ILIC

--- 0 - Épreuve pratique/2024/Sujets/24_NSI_29.py
+++ 0 - Épreuve pratique/2024/Sujets/24_NSI_29.py
@@ -1,17 +1,25 @@
+#maximilien ILIC
+def moyennes(n):
+    denominateur = 0
+    numerateur = 0
+    for i in range(len(n)):
+        denominateur += n[i][1]
+        numerateur += n[i][0] * n[i][1]
+    return numerateur/denominateur
+
+
 def ligne_suivante(ligne):
     '''Renvoie la ligne suivant ligne du triangle de Pascal'''
-    ligne_suiv = [...] 
-    for i in range(...): 
-        ligne_suiv.append(...) 
-    ligne_suiv.append(...) 
+    ligne_suiv = [1] 
+    for i in range(1, len(ligne)): 
+        ligne_suiv.append(ligne[i]+ligne[i-1]) 
+    ligne_suiv.append(1) 
     return ligne_suiv
 
 def pascal(n):
     '''Renvoie le triangle de Pascal de hauteur n'''
     triangle = [ [1] ]
-    for k in range(...): 
-        ligne_k = ... 
+    for k in range(n): 
+        ligne_k = ligne_suivante(triangle[k])
         triangle.append(ligne_k)
     return triangle
-
-
