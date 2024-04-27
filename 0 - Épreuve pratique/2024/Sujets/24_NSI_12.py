9c38db6169c4c7071fe323d870abfe7424e9bb82
maximilien-ilic <159456382+maximilien-ilic@users.noreply.github.com>
2024-04-27 18:13:13 +0200

    Update 24_NSI_12.py
    par maximilien ILic

--- 0 - Épreuve pratique/2024/Sujets/24_NSI_12.py
+++ 0 - Épreuve pratique/2024/Sujets/24_NSI_12.py
@@ -1,20 +1,31 @@
+#maximilien ILIC
 from random import randint
 
-def plus_ou_moins():
-    nb_mystere = randint(1, ...) 
-    nb_test = int(input("Proposez un nombre entre 1 et 99 : "))
-    compteur = ... 
+def tri_selection(tab):
+    n = len(tab)
+    for i in range(n):
+        min_indice = i
+        for j in range(i+1, n):
+            if tab[j] < tab[min_indice]:
+                min_indice = j
+        tab[i], tab[min_indice] = tab[min_indice], tab[i]
+    return tab
 
-    while nb_mystere != ... and compteur < ...: 
+
+def plus_ou_moins():
+    nb_mystere = randint(1,99)
+    nb_test = int(input("Proposez un nombre entre 1 et 99 : "))
+    compteur = 1
+
+    while nb_mystere != nb_test and compteur < 10 :
         compteur = compteur + 1
-        if nb_mystere ... nb_test: 
+        if nb_mystere > nb_test:
             nb_test = int(input("Trop petit ! Testez encore : "))
         else:
             nb_test = int(input("Trop grand ! Testez encore : "))
 
     if nb_mystere == nb_test:
-        print ("Bravo ! Le nombre était ", ...) 
-        print("Nombre d'essais: ", ...) 
+        print ("Bravo ! Le nombre etait ", nb_mystere)
+        print("Nombre d essais: ", compteur)
     else:
-        print ("Perdu ! Le nombre était ", ...) 
-
+        print ("Perdu ! Le nombre etait ", nb_mystere)
